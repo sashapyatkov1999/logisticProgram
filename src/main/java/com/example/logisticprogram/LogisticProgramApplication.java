c80ca16611d477bdfb60f030f0c4812be9435708
skidrow <skidrow1999@bk.ru>
2023-12-13 19:16:44 +0300

    d

---
+++ src/main/java/com/example/logisticprogram/LogisticProgramApplication.java
@@ -0,0 +1,13 @@
+package com.example.logisticprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticProgramApplication.class, args);
+    }
+
+}
