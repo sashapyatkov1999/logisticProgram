2605ebc891ae6822ffdb70d6656464823a082f3a
skidrow <skidrow1999@bk.ru>
2023-12-18 16:52:31 +0300

    2 задание

--- src/main/java/com/example/logisticprogram/config/SchemaInit.java
+++ src/main/java/com/example/logisticprogram/config/SchemaInit.java
@@ -3,11 +3,13 @@ package com.example.logisticprogram.config;
 
 import liquibase.change.DatabaseChange;
 import liquibase.integration.spring.SpringLiquibase;
-import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AbstractDependsOnBeanFactoryPostProcessor;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -24,35 +26,45 @@ import java.sql.Statement;
 @Slf4j
 @Configuration
 @ConditionalOnClass({SpringLiquibase.class, DatabaseChange.class})
-@ConditionalOnProperty(prefix = "spring.liquibase" , name = "enabled", matchIfMissing = true)
+@ConditionalOnProperty(prefix = "spring.liquibase", name = "enabled", matchIfMissing = true)
 @AutoConfigureAfter({DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @Import({SchemaInit.SpringLiquibaseDependsOnPostProcessor.class})
 public class SchemaInit {
 
     @Component
-    @ConditionalOnProperty(prefix = "spring.liquibase" , name = "enabled", matchIfMissing = true)
-    public class SpringLiquibaseDependsOnPostProcessor implements InitializingBean {
+    @ConditionalOnProperty(prefix = "spring.liquibase", name = "enabled", matchIfMissing = true)
+    public static class SchemaInitBean implements InitializingBean {
+
         private final DataSource dataSource;
-        private final  String liquiBaseSchema;
+        private final String liquiBaseSchema;
         private final String defaultSchema;
 
         @Autowired
-        public SchemaInitBean(DataSource dataSource, Value("LIQUIBASE") String liquiBaseSchema, @Value("REPOSITORY") String defaultSchema) {
+        public SchemaInitBean(DataSource dataSource, @org.springframework.beans.factory.annotation.Value("${spring.liquibase.liquibase-schema}") String liquiBaseSchema, @Value("${spring.liquibase.default-schema}") String defaultSchema) {
             this.dataSource = dataSource;
             this.liquiBaseSchema = liquiBaseSchema;
             this.defaultSchema = defaultSchema;
         }
 
-
         @Override
-        public void afterPropertiesSet()  {
+        public void afterPropertiesSet() {
             try (Connection conn = dataSource.getConnection();
-                 Statement statement = conn.createStatement())   {
-                statement.execute("CREATE SCHEMA IF NOT EXISTS" + liquiBaseSchema);
-                statement.execute("CREATE SCHEMA IF NOT EXISTS" + defaultSchema);
-            }catch (SQLException e){
-                throw new RuntimeException("Failed to create schema for liquibase ", e);
+                 Statement statement = conn.createStatement()) {
+                statement.execute("CREATE SCHEMA IF NOT EXISTS " + liquiBaseSchema);
+                statement.execute("CREATE SCHEMA IF NOT EXISTS " + defaultSchema);
+            } catch (SQLException e) {
+                throw new RuntimeException("Failed to create schema for liquibase", e);
             }
+        }
+    }
+
+
+    @ConditionalOnBean(SchemaInitBean.class)
+    static class SpringLiquibaseDependsOnPostProcessor extends AbstractDependsOnBeanFactoryPostProcessor {
+
+        SpringLiquibaseDependsOnPostProcessor() {
+            // Configure the 3rd party SpringLiquibase bean to depend on our SchemaInitBean
+            super(SpringLiquibase.class, SchemaInitBean.class);
         }
     }
 }
