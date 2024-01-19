744f746faae5f71eabb4dd5c3cbb1140304487de
skidrow <skidrow1999@bk.ru>
2024-01-19 18:31:50 +0300

    сделал UserDomainServiceTest , но не понял для чего private UserResponse getUserResponse() и private User getUser()

--- src/test/java/com/example/logisticprogram/service/domain/UserDomainServiceTest.java
+++ src/test/java/com/example/logisticprogram/service/domain/UserDomainServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.logisticprogram.service.domain;
 
+import com.example.logisticprogram.dto.request.user.UserAddRequest;
 import com.example.logisticprogram.dto.response.user.UserResponse;
 import com.example.logisticprogram.entity.User;
 import com.example.logisticprogram.mapper.user.UserMapper;
@@ -9,15 +10,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserDomainServiceTest {
@@ -54,14 +56,46 @@ class UserDomainServiceTest {
 
     @Test
     void getAllUsersTest() {
+        List<User> users = new ArrayList<>();
+        users.add(new User(1L));
+        users.add(new User(2L));
+
+        List<UserResponse> userResponses = new ArrayList<>();
+        userResponses.add(new UserResponse());
+        userResponses.add(new UserResponse());
+
+        when(userRepository.findAll()).thenReturn(users);
+        when(userResponseMapper.from(users)).thenReturn(userResponses);
+
+        List<UserResponse> result = service.getAllUsers();
+
+        assertEquals(userResponses, result);
+        assertNotNull(result);
+        verify(userRepository).findAll();
+        verify(userResponseMapper).from(users);
     }
 
     @Test
     void deleteUserTest() {
+
+        Long id = 1L;
+
+        service.deleteUser(id);
+        verify(userRepository).deleteById(id);
     }
 
     @Test
     void addUserTest() {
+        UserAddRequest userAddRequest = new UserAddRequest();
+        User user = new User(1L);
+        when(userMapper.from(userAddRequest)).thenReturn(user);
+        when(userRepository.save(user)).thenReturn(user);
+        Long id = service.addUser(userAddRequest);
+        assertEquals(1L,id);
+        verify(userMapper).from(userAddRequest);
+        verify(userRepository).save(user);
+
+
     }
 
 
