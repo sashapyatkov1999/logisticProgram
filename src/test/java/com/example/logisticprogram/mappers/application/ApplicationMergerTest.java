1a13b13405d80c0f7e4c6d67ca2603cebdb93f00
vyushin_aa <ledford1403@gmail.com>
2024-02-13 20:15:09 +0300

    Правки мапперов и иерджеров

--- src/test/java/com/example/logisticprogram/mappers/application/ApplicationMergerTest.java
+++ src/test/java/com/example/logisticprogram/mappers/application/ApplicationMergerTest.java
@@ -4,6 +4,7 @@ import com.example.logisticprogram.dto.request.application.ApplicationAddRequest
 import com.example.logisticprogram.entity.Application;
 import com.example.logisticprogram.mapper.application.ApplicationMapper;
 import com.example.logisticprogram.mapper.application.ApplicationMerger;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,6 +13,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.*;
@@ -22,10 +24,11 @@ class ApplicationMergerTest {
     private ApplicationMerger merger;
 
 
-    private final Long managerId = 1L;
-    private final Long driverId = 2L;
-    private final String name = "";
-    private final String description = "";
+    private final Long ID = 0L;
+    private final Long MANAGER_ID = 1L;
+    private final Long DRIVER_ID = 2L;
+    private final String NAME = "";
+    private final String DESCRIPTION = "";
     private final LocalDateTime CREATED = LocalDateTime.now();
     private final LocalDateTime MODIFIED = LocalDateTime.now();
 
@@ -36,28 +39,40 @@ class ApplicationMergerTest {
         var target = spy(target());
         var result = merger.merge(target,source);
 
-        assertNull(result.getId());
-        assertEquals(CREATED,result.getCreated());
-        assertEquals(MODIFIED,result.getModified());
-        assertEquals(managerId, result.getManager().getId());
-        assertEquals(driverId, result.getDriver().getId());
-        assertEquals(name, result.getName());
-        assertEquals(description, result.getDescription());
+        verify(source).getName();
+        verify(source).getDescription();
+        verify(source).getManagerId();
+        verify(source).getDriverId();
+
+        verify(target).setManager(any());
+        verify(target).setDriver(any());
+        verify(target).setName(any());
+        verify(target).setDescription(any());
+
+        verifyNoMoreInteractions(source, target);
+
+        assertThat(result.getId()).as("Id не совпадают").isEqualTo(ID);
+        assertThat(result.getName()).isEqualTo(NAME);
+        assertThat(result.getDescription()).isEqualTo(DESCRIPTION);
+        assertThat(result.getDriver().getId()).isEqualTo(DRIVER_ID);
+        assertThat(result.getManager().getId()).isEqualTo(MANAGER_ID);
+        assertThat(result.getCreated()).isEqualTo(CREATED);
+        assertThat(result.getModified()).isEqualTo(MODIFIED);
 
     }
 
     private ApplicationAddRequest getApplicationAddRequest(){
         return new ApplicationAddRequest()
-                .setManagerId(managerId)
-                .setDriverId(driverId)
-                .setName(name)
-                .setDescription(description);
+                .setManagerId(MANAGER_ID)
+                .setDriverId(DRIVER_ID)
+                .setName(NAME)
+                .setDescription(DESCRIPTION);
     }
 
 
     Application target(){
         return new Application()
-                .setId(1L)
+                .setId(ID)
                 .setCreated(CREATED)
                 .setModified(MODIFIED);
     }
--- src/test/java/com/example/logisticprogram/mappers/application/ApplicationResponseMapperTest.java
+++ src/test/java/com/example/logisticprogram/mappers/application/ApplicationResponseMapperTest.java
@@ -1,6 +1,5 @@
 package com.example.logisticprogram.mappers.application;
 
-import com.example.logisticprogram.dto.response.application.ApplicationResponse;
 import com.example.logisticprogram.dto.response.driver.DriverResponse;
 import com.example.logisticprogram.dto.response.user.UserResponse;
 import com.example.logisticprogram.entity.Application;
@@ -16,44 +15,50 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ApplicationResponseMapperTest {
     @Mock
     private DriverResponseMapper driverResponseMapper;
-
     @Mock
     private UserResponseMapper userResponseMapper;
     @InjectMocks
     private ApplicationResponseMapper mapper;
-    private final Long id = 1L;
-    Long driverId = 2L;
-    Long managerId = 3L;
-    String name = "";
-    String description = "";
-    LocalDateTime created = LocalDateTime.now();
-    LocalDateTime modified = LocalDateTime.now();
+    private final Long ID = 0L;
+    private final Long MANAGER_ID = 1L;
+    private final Long DRIVER_ID = 2L;
+    private final String NAME = "";
+    private final String DESCRIPTION = "";
+    private final LocalDateTime CREATED = LocalDateTime.now();
+    private final LocalDateTime MODIFIED = LocalDateTime.now();
+
 
     @Test
-     void ApplicationResponseTest() {
-        when(driverResponseMapper.from(any(Driver.class))).thenReturn(new DriverResponse());
-        when(userResponseMapper.from(any(User.class))).thenReturn(new UserResponse());
+    void ApplicationResponseTest() {
+        when(driverResponseMapper.from(any(Driver.class))).thenReturn(new DriverResponse().setId(DRIVER_ID));
+        when(userResponseMapper.from(any(User.class))).thenReturn(new UserResponse().setId(MANAGER_ID));
+
 
         var source = spy(source());
         var result = mapper.from(source);
 
-        assertEquals(response().getId(), result.getId());
-        assertEquals(response().getDriver(), result.getDriver());
-        assertEquals(response().getManager(), result.getManager());
-        assertEquals(response().getName(), result.getName());
-        assertEquals(response().getDescription(), result.getDescription());
-        assertEquals(response().getCreated(), result.getCreated());
-        assertEquals(response().getModified(), result.getModified());
+        assertThat(result.getId()).as("Id не совпадают").isEqualTo(ID);
+        assertThat(result.getName()).isEqualTo(NAME);
+        assertThat(result.getDescription()).isEqualTo(DESCRIPTION);
+        assertThat(result.getDriver().getId()).isEqualTo(DRIVER_ID);
+        assertThat(result.getManager().getId()).isEqualTo(MANAGER_ID);
+        assertThat(result.getCreated()).isEqualTo(CREATED);
+        assertThat(result.getModified()).isEqualTo(MODIFIED);
+
         verify(source).getDriver();
         verify(source).getId();
         verify(source).getManager();
@@ -61,30 +66,56 @@ class ApplicationResponseMapperTest {
         verify(source).getDescription();
         verify(source).getCreated();
         verify(source).getModified();
+        verify(driverResponseMapper).from(any(Driver.class));
+        verify(userResponseMapper).from(any(User.class));
 
         verifyNoMoreInteractions(source);
     }
-    ApplicationResponse response(){
-    ApplicationResponse response = new ApplicationResponse();
-        return response.setId(id)
-                .setDriver(new DriverResponse())
-                .setManager(new UserResponse())
-                .setName(name)
-                .setDescription(description)
-                .setCreated(created)
-                .setModified(modified);
+
+
+    @Test
+    void fromList() {
+
+        when(driverResponseMapper.from(any(Driver.class))).thenReturn(new DriverResponse().setId(DRIVER_ID));
+        when(userResponseMapper.from(any(User.class))).thenReturn(new UserResponse().setId(MANAGER_ID));
+
+        var source = spy(source());
+        var resultList = mapper.from(List.of(source, source, source));
+
+        assertThat(resultList).isNotEmpty();
+
+        var result = resultList.get(0);
+
+        assertThat(result.getId()).as("Id не совпадают").isEqualTo(ID);
+        assertThat(result.getName()).isEqualTo(NAME);
+        assertThat(result.getDescription()).isEqualTo(DESCRIPTION);
+        assertThat(result.getDriver().getId()).isEqualTo(DRIVER_ID);
+        assertThat(result.getManager().getId()).isEqualTo(MANAGER_ID);
+        assertThat(result.getCreated()).isEqualTo(CREATED);
+        assertThat(result.getModified()).isEqualTo(MODIFIED);
+
+        verify(source, times(3)).getDriver();
+        verify(source, times(3)).getId();
+        verify(source, times(3)).getManager();
+        verify(source, times(3)).getName();
+        verify(source, times(3)).getDescription();
+        verify(source, times(3)).getCreated();
+        verify(source, times(3)).getModified();
+        verify(driverResponseMapper, times(3)).from(any(Driver.class));
+        verify(userResponseMapper, times(3)).from(any(User.class));
+
+        verifyNoMoreInteractions(source);
     }
-    Application source() {
+
+
+    private Application source() {
         Application source = new Application();
-        return source.setId(id)
-                .setDriver(new Driver(driverId))
-                .setManager(new User(managerId))
-                .setName(name)
-                .setDescription(description)
-                .setCreated(created)
-                .setModified(modified);
-    }
-    ApplicationResponseMapper mapper(){
-       return new ApplicationResponseMapper(driverResponseMapper, userResponseMapper);
+        return source.setId(ID)
+                .setDriver(new Driver(DRIVER_ID))
+                .setManager(new User(MANAGER_ID))
+                .setName(NAME)
+                .setDescription(DESCRIPTION)
+                .setCreated(CREATED)
+                .setModified(MODIFIED);
     }
 }
