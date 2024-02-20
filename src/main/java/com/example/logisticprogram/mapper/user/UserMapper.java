19b6b660a459edd7b5117c4ee40f0378448e3bec
vyushin_aa <ledford1403@gmail.com>
2024-02-20 20:16:15 +0300

    Фикс

--- src/main/java/com/example/logisticprogram/mapper/user/UserMapper.java
+++ src/main/java/com/example/logisticprogram/mapper/user/UserMapper.java
@@ -3,9 +3,12 @@ package com.example.logisticprogram.mapper.user;
 import com.example.logisticprogram.dto.request.user.UserAddRequest;
 import com.example.logisticprogram.entity.User;
 import com.example.logisticprogram.entity.UserStatus;
+import com.example.logisticprogram.entity.UserStatusEnum;
 import com.example.logisticprogram.mapper.Mapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static com.example.logisticprogram.entity.UserStatusEnum.ENABLE;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +16,7 @@ public class UserMapper implements Mapper<User, UserAddRequest> {
     @Override
     public User from(UserAddRequest source) {
         return new User()
-                .setStatus(new UserStatus(source.getUserId()))
+                .setStatus(new UserStatus(ENABLE.getId()))
                 .setName(source.getName())
                 .setSurname(source.getSurname())
                 .setPhoneNumber(source.getPhoneNumber());
