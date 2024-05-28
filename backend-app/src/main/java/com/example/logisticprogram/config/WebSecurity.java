917dfb8c67e5f4dd6c9ee7a86cc2169fe0cbec05
vyushin_aa <ledford1403@gmail.com>
2024-05-28 09:28:07 +0300

    миграция на Postgresql

---
+++ backend-app/src/main/java/com/example/logisticprogram/config/WebSecurity.java
@@ -0,0 +1,93 @@
+package com.example.logisticprogram.config;
+
+import com.example.logisticprogram.service.domain.UserDomainService;
+import com.example.logisticprogram.service.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+
+import static com.example.logisticprogram.controller.UserController.USERS;
+import static com.example.logisticprogram.controller.UserController.USER_LOGIN;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class WebSecurity implements WebMvcConfigurer {
+
+    private final UserDomainService userDomainService;
+    private final JwtAuthenticationFilter filter;
+
+    private static final String API_URL_PATTERN = "api/v1/**";
+    @Bean
+    public SecurityFilterChain getSecurityFilterChain(HttpSecurity http,
+                                                      HandlerMappingIntrospector introspector) throws Exception {
+        MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.headers(headersConfigurer ->
+                headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+
+        http.authorizeHttpRequests(auth ->
+                auth
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern(USER_LOGIN)).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern(USERS)).permitAll()
+                        .anyRequest().authenticated()
+        );
+
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+        http.authenticationProvider(authenticationProvider());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+        return http.build();
+    }
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("POST", "GET");
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(passwordEncoder());
+        authenticationProvider.setUserDetailsService(userDomainService.userDetailsService());
+        return authenticationProvider;
+    }
+
+}
---
+++ backend-app/src/main/java/com/example/logisticprogram/mapper/application/ApplicationMapper.java
@@ -0,0 +1,22 @@
+package com.example.logisticprogram.mapper.application;
+
+import com.example.logisticprogram.dto.request.application.ApplicationAddRequest;
+import com.example.logisticprogram.entity.Application;
+import com.example.logisticprogram.entity.Driver;
+import com.example.logisticprogram.entity.User;
+import com.example.logisticprogram.mapper.Mapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ApplicationMapper implements Mapper<Application, ApplicationAddRequest> {
+    @Override
+    public Application from(ApplicationAddRequest source) {
+        return new Application()
+                .setManager(new User(source.getManagerId()))
+                .setDriver(new Driver(source.getDriverId()))
+                .setName(source.getName())
+                .setDescription(source.getDescription());
+    }
+}
